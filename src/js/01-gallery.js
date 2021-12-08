7c4a1c9b0475fe1be5509b32cc808a85860b7d67
Malanchenko Sergii <malan.sergii@gmail.com>
2021-12-08 13:34:04 +0200

    1-st done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const gallerySecond = document.querySelector(".gallery");
+
+function createElements(data) {
+    return data.map(({ description, original, preview }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join("")
+}
+
+gallerySecond.innerHTML = createElements(galleryItems);
+
+
+const imgAltOptions = {
+    captionsData: "alt",
+    captionDelay: 250
+}
+
+const lightbox = new SimpleLightbox('.gallery a', imgAltOptions);
