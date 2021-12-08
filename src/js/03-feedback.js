da4d93b414210c6e85e7d0c6b5a0debe308c3843
Malanchenko Sergii <malan.sergii@gmail.com>
2021-12-08 22:55:23 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+const throttle = require('lodash.throttle');
+
+
+const form = document.querySelector(".feedback-form");
+const mailInput = document.querySelector("input");
+const userMessage = document.querySelector("textarea");
+
+const userData = {
+    email: "",
+    message: ""
+};
+
+const parcedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+
+function isLocalStorage() {
+    if (localStorage.getItem("feedback-form-state")) {
+        mailInput.value = parcedData.email;
+        userMessage.value = parcedData.message;
+        userData.email = parcedData.email;
+        userData.message = parcedData.message;
+    }
+}
+isLocalStorage();
+
+
+mailInput.addEventListener("input", throttle((e) => {
+    userData.email = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+}, 500));
+
+
+userMessage.addEventListener("input", throttle((e) => {
+    userData.message = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(userData))
+}, 500));
+
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log(userData);
+    userData.email = "";
+    userData.message = "";
+    event.currentTarget.reset()
+    localStorage.removeItem("feedback-form-state");
+})
 
